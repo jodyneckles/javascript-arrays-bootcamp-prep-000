6b567658c7291fd50da9f98b051fda96724a8663
Jody Neckles <jody@technative.tv>
2018-10-17 10:31:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,13 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(array, element) {
-  var array = [1];
-    [element, ...array];
-  return array;
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1];
-  array = [element, ...array];
-  return array;
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
 }
