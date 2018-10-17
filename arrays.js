2ff6006ffaad13b40cde409673eb7a650eac2f1c
Jody Neckles <jody@technative.tv>
2018-10-17 11:39:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 }
 
-function removeElementFromEndOfArray() {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1);
 }
