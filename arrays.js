115b56cd3d094f129aff74aa5370d3246c715e79
Jody Neckles <jody@technative.tv>
2018-10-17 11:38:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray() {
-  array.slice(0, array.length - 1)
+  array.slice(0, array.length - 1);
 }
