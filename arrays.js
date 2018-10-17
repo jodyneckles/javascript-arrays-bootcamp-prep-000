c148fbfcb3945deeaca2177f8bdab75a5de5a9bd
Jody Neckles <jody@technative.tv>
2018-10-17 10:43:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var array = [];
+  var array = [1];
   array.unshift(element);
   return array;
 }
