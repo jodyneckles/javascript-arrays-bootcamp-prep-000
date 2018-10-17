2ccd95363d66c33f2d6e598ec180781434c52124
Jody Neckles <jody@technative.tv>
2018-10-17 10:00:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
-function addElementToBeginningOfArray(array, 'foo') {
+function addElementToBeginningOfArray(array, element('foo')) {
   var array = [1];
     [element, ...array];
   return array;
